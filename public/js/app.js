3544c0ac7bb44cf35541993eec2252a46ea68196
james <jamesofjungle@gmail.com>
2016-01-23 18:33:57 -0800

    added routes and partials

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,20 @@
 var app = angular.module('meanmaps', [
   'addCtrl',
   'geolocation',
-  'gservice'
+  'gservice',
+  'ngRoute'
 ]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/join', {
+      controller: 'addCtrl',
+      templateUrl: 'partials/addForm.html'
+    })
+    .when('/find', {
+      templateUrl: 'partials/queryForm.html'
+    })
+    .otherwise({
+      redirectTo: '/join'
+    })
+});

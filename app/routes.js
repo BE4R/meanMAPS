7bdc9176def85e68ade048d2aead8d7d0e92aa7a
james <jamesofjungle@gmail.com>
2016-01-23 19:55:09 -0800

    added filters to query POST

--- app/routes.js
+++ app/routes.js
@@ -1,42 +1,115 @@
 // Dependencies
 var mongoose = require('mongoose');
-var User     = require('./model.js');
+var User = require('./model.js');
 
 // Open app routes
-module.exports = function(app) {
+module.exports = function (app) {
   // GET ROUTES
   // all users
-  app.get('/users', function(req, res){
-    
+  app.get('/users', function (req, res) {
+
     // Uses the mongoose schema to run the search with empty conditions
     var query = User.find({});
-    query.exec(function(err, users) {
-      if(err){
-        res.send(err); 
+    query.exec(function (err, users) {
+      if (err) {
+        res.send(err);
       }
       console.log('Getting all users!');
       // If no users are found, it will respond with a JSON of all users
       res.json(users);
     });
   });
-  
+
   // POST ROUTES
   // save user
-  app.post('/users', function(req, res){
-    
+  app.post('/users', function (req, res) {
+
     // Creates a new User based on the request body
     var newuser = new User(req.body);
-    
+
     // save the new user in the DB
-    newuser.save(function(err){
-      if(err){
-        res.send(err); 
+    newuser.save(function (err) {
+      if (err) {
+        res.send(err);
       }
-      
+
       // if no errors, respond with a JSON of the user
       console.log('user created!');
       res.json(req.body);
     });
   });
-  
-};
+
+  // Retrieves JSON records for all users who meet query conditions
+  app.post('/query/', function (req, res) {
+
+    // Grab all of the query parameters from the body.
+    var lat = req.body.latitude;
+    var long = req.body.longitude;
+    var distance = req.body.distance;
+    var male = req.body.male;
+    var female = req.body.female;
+    var other = req.body.other;
+    var minAge = req.body.minAge;
+    var maxAge = req.body.maxAge;
+    var favLang = req.body.favlang;
+    var reqVerified = req.body.reqVerified;
+
+    // Opens a generic Mongoose Query. Depending on the post body we will...
+    var query = User.find({});
+
+    // ...include filter by Max Distance (converting miles to meters)
+    if (distance) {
+
+      // Using MongoDB's geospatial querying features. (Note how coordinates are set [long, lat]
+      query = query.where('location').near({
+        center: {
+          type: 'Point',
+          coordinates: [long, lat]
+        },
+
+        // Converting meters to miles. Specifying spherical geometry (for globe)
+        maxDistance: distance * 1609.34,
+        spherical: true
+      });
+    }
+
+    // ...include filter by Gender (all options)
+    if (male || female || other) {
+      query.or([{
+        'gender': male
+        }, {
+        'gender': female
+        }, {
+        'gender': other
+        }]);
+    }
+
+    // ...include filter by Min Age
+    if (minAge) {
+      query = query.where('age').gte(minAge);
+    }
+
+    // ...include filter by Max Age
+    if (maxAge) {
+      query = query.where('age').lte(maxAge);
+    }
+
+    // ...include filter by Favorite Language
+    if (favLang) {
+      query = query.where('favlang').equals(favLang);
+    }
+
+    // ...include filter for HTML5 Verified Locations
+    if (reqVerified) {
+      query = query.where('htmlverified').equals("Yep (Thanks for giving us real data!)");
+    }
+    // Execute Query and Return the Query Results
+    query.exec(function (err, users) {
+      if (err)
+        res.send(err);
+
+      // If no errors, respond with a JSON of all users that meet the criteria
+      res.json(users);
+    });
+  });
+}
--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -3,47 +3,50 @@ var addCtrl = angular.module('addCtrl', [
   'gservice'
 ]);
 
-addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice){
-  
+addCtrl.controller('addCtrl', function ($scope, $http, $rootScope, geolocation, gservice) {
+
   // initialize the variables
   $scope.formData = {};
   var coords = [];
   var lat = 0;
   var long = 0;
-  
+
   // set initial coordinates to center of the US
   $scope.formData.latitude = 39.500;
   $scope.formData.longitude = -98.350;
-  
+
   // Get User's actual coordinates based on HTML5 at window load
-  geolocation.getLocation().then(function(data){
+  geolocation.getLocation().then(function (data) {
 
     // Set the latitude and longitude equal to the HTML5 coordinates
-    coords = {lat:data.coords.latitude, long:data.coords.longitude};
+    coords = {
+      lat: data.coords.latitude,
+      long: data.coords.longitude
+    };
 
     // Display coordinates in location textboxes rounded to three decimal points
-    $scope.formData.longitude = coords.long.toFixed(3);
-    $scope.formData.latitude = coords.lat.toFixed(3);
+    $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
+    $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
 
     // Display message confirming that the coordinates verified.
     $scope.formData.htmlverified = "Yep (Thanks for giving us real data!)";
 
-    gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+    gservice.refresh(coords.lat, coords.long);
 
   });
-  
+
   //Get coordinates based on mouse click.
-  $rootScope.$on("clicked", function(){
-    $scope.$apply(function(){
-      $scope.formData.latitude = gservice.clickLat.toFixed(3);
-      $scope.formData.longitude = gservice.clickLong.toFixed(3);
+  $rootScope.$on("clicked", function () {
+    $scope.$apply(function () {
+      $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
+      $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
       $scope.formData.htmlverified = "Nope (Thanks for spamming my map...)";
     });
   });
-  
+
   // creates the user
-  $scope.createUser = function() {
-    
+  $scope.createUser = function () {
+
     // retrieves the values from the form
     var userData = {
       username: $scope.formData.username,
@@ -53,21 +56,20 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
       location: [$scope.formData.longitude, $scope.formData.latitude],
       htmlverified: $scope.formData.htmlverified
     };
-    
+
     $http.post('/users', userData)
-      .success(function(data){
+      .success(function (data) {
         console.log('user created!');
+        // Refresh the map with new data
+        gservice.refresh(parseFloat($scope.formData.latitude), parseFloat($scope.formData.longitude));
         // on success, reset the formdata
         $scope.formData.username = "";
         $scope.formData.gender = "";
         $scope.formData.age = "";
         $scope.formData.favlang = "";
-      
-        // Refresh the map with new data
-        gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
       })
-      .error(function(data){
+      .error(function (data) {
         console.log('Error: ' + data);
-    });
+      });
   };
 });
